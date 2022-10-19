0ccd68c722e1798bab86fb70911b142556e41162
Rodence Atacador <113341443+rodence@users.noreply.github.com>
2022-10-19 22:36:00 +0800

    Add files via upload

---
+++ function.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/home")
+def main():
+    return render_template("index2.html")
+
+
+@app.route("/info")
+def info():
+    return render_template("aass.html")
+
+
+@app.route("/planets")
+def planets():
+    return render_template("apam.html")
+
+@app.route("/view")
+def view():
+    return render_template("vtss.html")
+
+@app.route("/images")
+def images():
+    return render_template("sai.html")
+
+@app.route("/quiz")
+def quiz():
+    return render_template("pq.html")
+
+
+@app.route("/facts")
+def facts():
+    return render_template("if.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
